e3da9038b03e1245ebed66e686d8a4851383d906
kaloyan <milchevkaloian@gmail.com>
2025-02-02 11:50:09 +0200

    fix(database host) Fix database host name back to service name

--- server/server/settings.py
+++ server/server/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "NAME": "rideon_db",
         "USER": "postgres",
         "PASSWORD": "1234qwer",
-        "HOST": "localhost",
+        "HOST": "postgres",
         "PORT": "5432",
     }
 }
