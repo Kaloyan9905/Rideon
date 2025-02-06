aaac8eb9a39cb3d2f1cce00489dbec9ef5407605
kaloyan <milchevkaloian@gmail.com>
2025-02-06 17:16:40 +0200

    feat(swagger) Swagger API documentation

--- server/server/urls.py
+++ server/server/urls.py
@@ -2,12 +2,29 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Rideon API",
+        default_version='v1',
+        description="API documentation for Rideon",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authenticate/', include('server.authentication.urls')),
     path('api/', include('server.accounts.urls')),
     path('pass/', include('server.passes.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
 ]
 
 if settings.DEBUG:  # Serve media files only in development mode
