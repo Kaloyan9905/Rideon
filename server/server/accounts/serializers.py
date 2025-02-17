4dd9f66120b703ecfbe897b7d911fc823843d706
kaloyan <milchevkaloian@gmail.com>
2025-02-17 19:05:31 +0200

    fix(pass-serializers) add card and ticket serializers to the profile

--- server/server/accounts/serializers.py
+++ server/server/accounts/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 from server.accounts.models import UserProfile
+from server.passes.serializers import CardSerializer, TicketSerializer
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    card = serializers.SerializerMethodField()
+    tickets = serializers.SerializerMethodField()
+
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        fields = ['id', 'first_name', 'last_name', 'card', 'tickets']
         read_only_fields = ['user']
+
+    @staticmethod
+    def get_user_card(card):
+        return CardSerializer(card.card_files, many=True).data
+
+    @staticmethod
+    def get_user_tickets(ticket):
+        return TicketSerializer(ticket.ticket_files, many=True).data
