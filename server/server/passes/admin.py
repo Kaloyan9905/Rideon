801f8bef9f2b26c5a3e06d46c80507f1d77375d4
b0jkata14 <bobovasev@gmail.com>
2025-03-24 15:52:42 +0200

    feat(unfold): integrate unfold for improving the admin panel

--- server/server/passes/admin.py
+++ server/server/passes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from server.passes.models import Ticket, Card
+
+
+@admin.register(Ticket)
+class TicketAdmin(ModelAdmin):
+    list_display = ("serial_number", "owner", "expires_at", "is_used")
+    list_filter = ("is_used",)
+    readonly_fields = ("qr_code",)
+
+
+@admin.register(Card)
+class CardAdmin(ModelAdmin):
+    list_display = ("serial_number", "owner", "expires_at", "last_used_at")
+    readonly_fields = ("last_used_at",)
